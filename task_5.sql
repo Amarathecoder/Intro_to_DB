ba007f362b0f3ba1f1f58507e13f15e65bb56115
Amarachukwu Ekwugha <amarachukwuekwugha@gmail.com>
2025-07-04 23:59:02 +0100

    Add SQL script to insert a customer record

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 USE alx_book_store;
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Chioma Okeke', 'chiomaokeke@gmail.com', '2 Umuodu Kamali Estate.');
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
